e01486d8df0b20d0d5b8936ab4b6539805c0a2f9
Elss Zion <elsszion@gmail.com>
2025-01-24 15:34:07 +0800

    update sha256

--- ndkctl.rb
+++ ndkctl.rb
@@ -2,7 +2,7 @@ class Ndkctl < Formula
   desc "A simple command-line tool to manage multiple NDK versions"
   homepage "https://github.com/ELSS-ZION/ndkctl"
   url "https://github.com/ELSS-ZION/ndkctl/archive/refs/tags/v1.0.0.zip"
-  sha256 "5807870145e9885928f354d958c13e04b0bec16f847761d1323ea4256f6b124d"
+  sha256 "692cc3059ee4e3fdc2613006c4bc25a3ac7070f080e51b3f51d512965d3c2cc1"
 
   def install
     bin.install "bin/ndkctl"
